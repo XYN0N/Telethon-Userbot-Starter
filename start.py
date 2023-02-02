8be4fc421b84a9758f5e1c9ffe24860e88e84fe4
macs <46033510+XYN0N@users.noreply.github.com>
2023-02-02 04:18:43 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,15 +1,27 @@
 import telethon
 
 # replace with your own API ID and HASH
-api_id = apid  # from https://my.telegram.org/
-api_hash =  'apihash'  #from https://my.telegram.org/
+api_id = Your API ID
+api_hash = 'Your API HASH'
+
+# replace with the owner's user ID
+OWNER_ID = #Your ADMIN ID
 
 client = telethon.TelegramClient('session_name', api_id, api_hash)
 
 @client.on(telethon.events.NewMessage)
 async def handler(event):
-    if event.message.text and event.message.text.startswith("#ping"):
-        await event.respond("Pong!")
+    print("Received message:", event.message.text)
+    if event.message.from_id.user_id != OWNER_ID:
+        return
+
+    if not event.message.text or not event.message.text.startswith("#ping"):
+        return
+
+    print("Sending response...")
+    await event.respond("pong!")
+    print("Response sent.")
 
 client.start()
+print("Client started :D")
 client.run_until_disconnected()
